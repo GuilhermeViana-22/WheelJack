3737525ab0f4366d58d2e74c97ac7dbe215b9ed7
GuilhermeViana-22 <gguicido.viana@gmail.com>
2025-04-09 11:43:01 -0300

    deixando o ul comentado

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -82,15 +82,15 @@
               </div>
               <span class="group-hover:text-white transition-colors duration-300">thonycarlos2000@gmail.com</span>
             </li>
-            <li class="text-white/70 flex items-start group mt-4">
+            <!-- <li class="text-white/70 flex items-start group mt-4">
               <div class="icon-container mr-3 mt-0.5">
                 <svg class="w-5 h-5 text-white/70 group-hover:text-white transition-colors duration-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                 </svg>
               </div>
-             
-            </li>
+              <span class="group-hover:text-white transition-colors duration-300">Av. Exemplo, 123 - São Paulo/SP</span>
+            </li> -->
           </ul>
         </div>
 
